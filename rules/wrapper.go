62988bd1a9e79ebbd1c66392685381078b8ef23d
Zane Starr <zcstarr@aperitur2.local>
2026-05-14 22:21:30 -0700

    feat: add support for recommended yaml
    This PR adds support for adding a default set of lint rules that
    can be overriden by additional rules that are specified.

---
+++ rules/wrapper.go
@@ -0,0 +1,78 @@
+package rules
+
+import (
+	"fmt"
+	"io/fs"
+	"maps"
+	"os"
+	"path/filepath"
+
+	"github.com/open-rpc/openrpc-linter/types"
+	"gopkg.in/yaml.v3"
+)
+
+type RulesWrapper struct {
+	Extends []types.RuleDefaults  `yaml:"extends,omitempty"`
+	Rules   map[string]types.Rule `yaml:"rules,omitempty"`
+}
+
+func (rw *RulesWrapper) CheckRules() error {
+	if len(rw.Rules) == 0 && len(rw.Extends) == 0 {
+		return fmt.Errorf("no rules to merge, rules and extends are empty invalid rules")
+	}
+	return nil
+}
+
+func (rw *RulesWrapper) ResolvedRules() (map[string]types.Rule, error) {
+
+	if rw.Rules == nil {
+		rw.Rules = make(map[string]types.Rule)
+	}
+
+	merged, err := getExtendedRules(rw.Extends)
+	if err != nil {
+		return nil, err
+	}
+	maps.Copy(merged, rw.Rules) // user rules win
+	return merged, nil
+}
+
+func getExtendedRules(exts []types.RuleDefaults) (map[string]types.Rule, error) {
+	out := make(map[string]types.Rule)
+	for _, ext := range exts {
+		switch ext {
+		case types.RuleExtensionRecommended:
+			w, err := LoadRulesFile(GetRuleDefaultsFS(), string(ext)+".yaml")
+			if err != nil {
+				return nil, fmt.Errorf("loading rule extension %q: %w", ext, err)
+			}
+			maps.Copy(out, w.Rules)
+		default:
+			return nil, fmt.Errorf("unknown rule extension %q", ext)
+		}
+	}
+	return out, nil
+}
+
+func LoadRulesFile(fsys fs.FS, name string) (*RulesWrapper, error) {
+	b, err := fs.ReadFile(fsys, name)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading rules file: %v\n", err)
+		return nil, err
+	}
+	var rw RulesWrapper
+	err = yaml.Unmarshal(b, &rw)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error parsing rules file: %v\n", err)
+		return nil, err
+	}
+	return &rw, nil
+}
+
+func LoadRulesFileFromPath(path string) (*RulesWrapper, error) {
+	abs, err := filepath.Abs(path)
+	if err != nil {
+		return nil, err
+	}
+	return LoadRulesFile(os.DirFS(filepath.Dir(abs)), filepath.Base(abs))
+}
